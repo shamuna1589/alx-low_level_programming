f2e31e90dfee8853355a6f62f752b1847b2cebc5
shamuna1589 <shamunaabdul@gmail.com>
2023-08-10 16:59:10 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Using puts function in a line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}    
