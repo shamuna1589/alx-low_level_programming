12130afe5749b3ad71ede6db6cd30d2c03236b5a
shamuna1589 <shamunaabdul@gmail.com>
2023-08-11 07:19:23 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char digit = '0';
+while (digit <= '9')
+{
+putchar(digit);
+digit++;
+}
+putchar('\n');
+return (0);
+}
+
