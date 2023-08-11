410b55848659465faccb10957e24d9f13081d8df
shamuna1589 <shamunaabdul@gmail.com>
2023-08-11 06:31:47 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
-#include< unistd.h>
+I#include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - a script for printing standard errror
- * Return 1 (success)
+ * main - Prints a specific text to the standard error
+ *
+ * Return: Always 1 (Error)
  */
-int mani (void)
+int main(void)
 {
-fput("and that piece of art is useful:
-\" - Dora Korpar, 2015-10-1
-\n"stdout);	
-return: (1)
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+59);
+return (1);
+}
+
