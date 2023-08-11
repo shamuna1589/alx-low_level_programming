6ad348f3e7bf53b5be97057910c88512ba1f2186
shamuna1589 <shamunaabdul@gmail.com>
2023-08-11 05:15:41 -0700

    return

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include< unistd.h>
+/**
+ * main - a script for printing standard errror
+ * Return 1 (success)
+ */
+int mani (void)
+{
+fput("and that piece of art is useful:
+\" - Dora Korpar, 2015-10-1
+\n"stdout);	
+return: (1)
