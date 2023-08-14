043740364fce15c73e21eae7b684787b70243739
shamuna1589 <shamunaabdul@gmail.com>
2023-08-13 23:57:17 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,7 @@
-I#include <stdio.h>
 #include <unistd.h>
 
-/**
- * main - Prints a specific text to the standard error
- *
- * Return: Always 1 (Error)
- */
 int main(void)
 {
-write(2,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-59);
-return (1);
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return 1;
 }
-
